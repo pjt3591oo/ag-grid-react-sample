93935f3638618a820cfabbd477d8c972a2b813cd
pjt3591oo <pjt3591oo@gmail.com>
2021-06-30 16:59:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { rowData } from 'react';
+import { render } from 'react-dom';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
+const App = () => {
+   const rowData = [
+       {make: "Toyota", model: "Celica", price: 35000},
+       {make: "Ford", model: "Mondeo", price: 32000},
+       {make: "Porsche", model: "Boxter", price: 72000}
+   ];
+
+
+   const onCellValueChanged = ({colDef, value}) => {
+    console.log(colDef.field, value);
+   }
+
+
+   return (
+       <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+          <AgGridReact
+            rowData={rowData}
+            defaultColDef={{
+              editable: true,
+            }}
+            suppressRowTransform={true}
+            suppressRowClickSelection={true}
+            rowSelection={'multiple'}
+            onCellValueChanged={onCellValueChanged}
+
+          >
+              <AgGridColumn field="make"></AgGridColumn>
+              <AgGridColumn field="model"></AgGridColumn>
+              <AgGridColumn field="price"></AgGridColumn>
+          </AgGridReact>
+
+       </div>
+   );
+};
 export default App;
